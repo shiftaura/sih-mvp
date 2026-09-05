836e167f2ee66c7aded020084dad1a74097f8890
AVNISH KUMAR VERMA <a33010171@gmail.com>
2026-09-05 09:18:10 +0530

    createing get project and post project

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use("/auth", require("./authRoutes.js"));
+router.use("/projects", require("./projectRoutes.js"));
 module.exports = router;
