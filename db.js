9d52898dc3b600f060d9f48a71acc16bb0dc9f9e
shiftaura <shiftaura@zohomail.in>
2026-09-16 22:19:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/nlas-sih');
+        const conn = await mongoose.connect(process.env.URI || 'mongodb://localhost:27017/nlas-sih');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
