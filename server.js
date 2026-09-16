c99565dacd3edc4b46fd4ff4a432595bc0c30f27
shiftaura <shiftaura@zohomail.in>
2026-09-16 23:51:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 const connectDB = require("./db"); // Updated DB connection
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors(
     {origin:"https://nlas.shiftaura.in",
         credentials:true,
     }
 ));
-
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
